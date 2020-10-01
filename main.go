b354c48f731aaa9342d4d5b1cf4346d9b2b77c08
wizenith <wizenithink@gmail.com>
2020-10-01 22:50:51 +0800

    update main.go adding starter

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
-// Package main provides ...
 package billutilities
 
 import "fmt"
 
 const MAX int = 9999
 
-func Greeting() {
-	fmt.Println("Hi from billutilities")
-}
-func Mylab() {
-	fmt.Println("LAB from github.com/wizenith/billutilities")
-}
-func ThirdFunc() {
-	fmt.Println("xxx")
+func First() {
+	fmt.Println("formal start")
 }
