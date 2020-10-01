e28551361f36b3030c65844dcca800de10fb8460
wizenith <wizenithink@gmail.com>
2020-10-01 22:47:14 +0800

    update main.go

--- main.go
+++ main.go
@@ -8,3 +8,9 @@ const MAX int = 9999
 func Greeting() {
 	fmt.Println("Hi from billutilities")
 }
+func Mylab() {
+	fmt.Println("LAB from github.com/wizenith/billutilities")
+}
+func ThirdFunc() {
+	fmt.Println("xxx")
+}
