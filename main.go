5e899995cc40f71b2069a7f73eb72138f11b35df
wizenith <wizenithink@gmail.com>
2020-10-20 18:52:05 +0800

    update stringtoslicebytes and slicebyetostring

--- main.go
+++ main.go
@@ -7,9 +7,29 @@ import (
 	"regexp"
 	"strings"
 	"time"
+	"unsafe"
 )
 
-//test
+func StringToSliceBytes(str string) []byte {
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&str))
+	bh := reflect.SliceHeader{
+		Data: sh.Data,
+		Len:  sh.Len,
+		Cap:  sh.Len,
+	}
+
+	return *(*[]byte)(unsafe.Pointer(&bh))
+}
+
+func SliceByteToString(b []byte) string {
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := reflect.StringHeader{
+		Data: bh.Data,
+		Len:  bh.Len,
+	}
+	return *(*string)(unsafe.Pointer(&sh))
+}
+
 func CronFnEveryBy(t time.Duration, fn func(...interface{}), params ...interface{}) {
 	for range time.Tick(t) {
 		fn(params...)
