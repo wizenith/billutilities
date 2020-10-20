0414b74cf8e0131999adfee2483e3500bd32825d
wizenith <wizenithink@gmail.com>
2020-10-20 19:38:18 +0800

    test123 123 123

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// ✓ [add aaa func with test123 as testing gf]
+//todo [✓] test123 123 123
 package billutilities
 
 import (
@@ -11,7 +11,7 @@ import (
 	"unsafe"
 )
 
-func aaa() { "test123" }
+func aaa() { "test123 123 123" }
 
 func StringToSliceBytes(str string) []byte {
 	sh := (*reflect.StringHeader)(unsafe.Pointer(&str))
